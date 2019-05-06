6352fc6cf44db8486cf6b9a891a8eb59360f8016
ceboulba <antoine@archipicture.fr>
2019-05-07 01:58:52 +0200

    1 file modified, 1 deleted

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import Title from './title'
-import BtnPlay from './btnPlay'
-import Count from './count'
-import { Button, Drawer, Icon } from 'antd'
+import { Layout, Content, Row, Col, Button, Icon } from 'antd'
 import 'antd/dist/antd.css';
 
 
@@ -12,95 +10,27 @@ export default class App extends Component {
     this.state = {
       sessionTime: 25,
       breakTime: 5,
-      time: 1000,
-      second: Math.round(990 / 16.67)
     }
   }
 
   handleClick = (event) => {
-  event.preventDefault()
-  console.log(event.currentTarget.dataset)
-  this.setState( { sessionTime : this.state.sessionTime += 1})
+    event.preventDefault()
+    console.log(event)
   }
-
-  oneMinute = (event) => {
-    while (this.state.time > 0) {
-      setInterval(() => this.setState({ time: this.state.time -= 100 }), 100)
-    }
-    console.log('theEvent come', event)
-    const time = this.state.sessionTime * 1000
-    console.log('variable Time', this.state.time)
-  }
-
-  addOne = () => {
-    console.log('addOneAction')
-  }
-
-   showDrawer = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  onClose = () => {
-    this.setState({
-      visible: false,
-    });
-  };
 
   render() {
     return (
-      <div className="hero">
-        <div className="hero-body">
-          <div className="container has-text-centered">
-            <Drawer
-              title="Basic Drawer"
-              placement={this.state.placement}
-              closable={false}
-              onClose={this.onClose}
-              visible={this.state.visible}
-            >
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-            </Drawer>
-            <Title title="Fcc Pomodoro Clock" />
-            <div className="has-text-centered">
-              <div>
-                <Button type='primary' etat='sessionTime' onClick={this.showDrawer}>
-                  bim
-                </Button>
-                <Button btnName='addSessionTime' action={this.handleClick} value='add' etat='sessionTime' />
-                <Button btnName='newBtn' action={this.addOne} />
-                <Count />
-              </div>
-            </div>
-            <div>
-            <span onClick={this.handleClick} quoi="LaData">
-            <Icon type="plus-circle"/>
-            </span>
-              <p className="subtitle is-inline" style={{margin:'1em'}}>
-                {this.state.sessionTime}
-              </p>
-            <Icon type="minus-circle" />
-            </div>
-
-            <div className="has-text-centered">
-              <div>
-                <Button btnName='subBreakTime' action={this.handleClick} value='sub' etat='breakTime' />
-                <Button btnName='addBreakTime' action={this.handleClick} value='add' etat='breakTime' />
-              </div>
-              <h2 className="subtitle has-text-centered">
-                {this.state.breakTime}
-              </h2>
-            </div>
-            <BtnPlay action={this.oneMinute} />
-            <h2 className="subtitle has-text-centered">
-              {this.state.second}
-            </h2>
-          </div>
-        </div>
-      </div>
+      <Fragment>
+        <Layout>
+          <Content>
+            <Row>
+              <Col span={12} offset={6}>
+                <Title title="Fcc Pomodoro Clock" />
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      </Fragment>
     );
   }
 }
--- count.js
+++
@@ -1,9 +0,0 @@
-import React,{ Fragment } from 'react';
-
-export default () => {
-  return (
-    <Fragment>
-      <h3>1000</h3>
-    </Fragment>  
-  )
-}
