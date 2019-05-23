4351bd264a7c50f4d762e540ee151e0a2d5e067f
ceboulba <antoine@archipicture.fr>
2019-05-23 23:38:19 +0200

    1 file modified

--- app.js
+++ app.js
@@ -4,41 +4,36 @@ import Title from './title'
 import 'antd/dist/antd.css';
 import { Button, Icon, Layout, Row, Col } from 'antd';
 
-const {
-  Header, Footer, Sider, Content
-} = Layout;
 
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       sessionTime: 25,
       breakTime: 5,
+      time: 0
     }
   }
 
+  addOne = () => {
+    this.setState({ time: this.state.time + 1 })
+  }
 
 
   render() {
     return (
       <Fragment>
         <Row className="ant-row">
-          <Col className="ant-col ant-col-12">
-            <h1 className='ant-typography ant-typography-danger'>Pomodoro Clock</h1>
-          </Col>
-          <Col className="ant-col ant-col-6">
-            <h1 className='ant-typography ant-typography-danger'>Pomodoro Clock</h1>
-          </Col>
           <Col className="ant-col ant-col-3">
             <h1 className='ant-typography ant-typography-danger'>Pomodoro Clock</h1>
           </Col>
         </Row>
         <Row>
-      <Col span={12} offset={6}>
-        col-12 col-offset-6
-      </Col>
-    </Row>
+        </Row>
+        <button className="button" onClick={this.addOne}>AddOne</button>
+        <p>{this.state.time}</p>
       </Fragment>
     );
   }
