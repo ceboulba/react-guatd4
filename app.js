dee0636b737070c364f278e28cc0565e336e854b
ceboulba <antoine@archipicture.fr>
2019-05-26 11:12:30 +0200

    3 files modified

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 import React, { Component, Fragment } from 'react'
-// import { Header, Layout, Content, Row, Col } from 'antd'
 import Title from './title'
-import 'antd/dist/antd.css';
-import { Button, Icon, Layout, Row, Col } from 'antd';
-
 
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       sessionTime: 25,
       breakTime: 5,
@@ -17,21 +12,18 @@ export default class App extends Component {
     }
   }
 
-  // addOne = s  etInterval( ()=>{alert('f')}, 2500)
-
-
-
   render() {
     return (
       <Fragment>
-        <Row className="ant-row">
-          <Col className="ant-col ant-col-3">
-            <h1 className='ant-typography ant-typography-danger'>Pomodoro Clock</h1>
-          </Col>
-        </Row>
-        <Row>
-        </Row>
-        <button className="button" onClick={}>AddOne</button>
+        <div className="ant-row">
+          <div className="ant-col ant-col-3">
+            <h1 className='ant-typography ant-typography-danger'>Pomodoro Clock
+            </h1>
+          </div>
+        </div>
+        <div>
+        </div>
+        <button type="button" className="ant-btn" onClick={}>AddOne</button>
         <p>{this.state.time}</p>
       </Fragment>
     );
