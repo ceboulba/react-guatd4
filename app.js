95629b1e922863f1870e1d048a8e0db050b3b99c
ceboulba <antoine@archipicture.fr>
2019-05-29 15:52:59 +0200

    111

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import React, { Component, Fragment } from 'react'
-import Title from './title'
-import { Card, Button, Row, Col } from 'antd'
+import {
+  Card,
+  Button,
+  Row,
+  Col,
+  Layout,
+  Content,
+  Header,
+  Sider,
+  Footer,
+} from 'antd'
 
 export default class App extends Component {
   constructor(props) {
@@ -15,50 +24,44 @@ export default class App extends Component {
     }
   }
 
-  this.timer = () => {
-    console.log('this = ', this)
-  }
-
   render() {
     return (
-      <Fragment>
-        <div className="ant-layout">
-          <div className="ant-layout-header">
-            <Title title="pomodoro Clock" />
+      <div>
+        <div>
+          <div>
+            <h1>Header</h1>
           </div>
-          <div className="ant-layout-content">
-            <div className="container">
-              <div id="break-label" className="ant-typography">
-                break length
-              </div>
-              <Button
-                id="break-decrement"
-                type="dashed"
-                shape="circle"
-                icon="plus"
-                onClick={event => {
-                  this.setState({ breakLength: (this.state.breakLength += 1) })
-                }}
-              />
-              <Button
-                id="break-increment"
-                type="dashed"
-                shape="circle"
-                icon="minus"
-                onClick={event => {
-                  this.state.breakLength > 0
-                    ? this.setState({
-                        breakLength: (this.state.breakLength -= 1),
-                      })
-                    : null
-                }}
-              />
-              <p id="break-length">{this.state.breakLength}</p>
-            </div>
+          <div>
+            <div id="break-label">break length</div>
+            <Button
+              id="break-decrement"
+              type="dashed"
+              shape="circle"
+              icon="plus"
+              onClick={event => {
+                this.setState({ breakLength: (this.state.breakLength += 1) })
+              }}
+            />
+            <Button
+              id="break-increment"
+              type="dashed"
+              shape="circle"
+              icon="minus"
+              onClick={event => {
+                this.state.breakLength > 0
+                  ? this.setState({
+                      breakLength: (this.state.breakLength -= 1),
+                    })
+                  : null
+              }}
+            />
+            <p id="break-length">{this.state.breakLength}</p>
+          </div>
+        </div>
 
-            <div id="session-label" className="ant-typography">
-              session label
-            </div>
+        <div>
+          <div>
+            <div id="session-label">session label</div>
             <Button
               id="session-decrement"
               type="dashed"
@@ -87,15 +90,19 @@ export default class App extends Component {
             <p id="timer-label">{this.state.timerLabel[0]}</p>
             <p id="timer-left">{Math.floor(this.state.seconds * 1.667)}</p>
             <p id="time-left">{this.state.timeLeft}</p>
-            <Button id="start_stop" type="dashed" shape="round">
-              START
-            </Button>
-            <Button id="reset" type="dashed" shape="round">
-              RESET
-            </Button>
+          </div>
+          <div>
+            <div className="container">
+              <Button id="start_stop" type="dashed" shape="round">
+                START
+              </Button>
+              <Button id="reset" type="dashed" shape="round">
+                RESET
+              </Button>
+            </div>
           </div>
         </div>
-      </Fragment>
+      </div>
     )
   }
 }
